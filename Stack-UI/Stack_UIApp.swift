0e8e053ea8cb71d35a0eb2e4b2c47ccbcc3ba1ca
mTayo <m.adinlewa@gmail.com>
2024-06-21 13:12:03 +0100

    Initial Commit

---
+++ Stack-UI/Stack_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stack_UIApp.swift
+//  Stack-UI
+//
+//  Created by Adinlewa Olutayo on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Stack_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
